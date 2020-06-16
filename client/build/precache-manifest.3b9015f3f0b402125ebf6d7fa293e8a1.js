9743103882533ac0ce171ed008791f2bad344eb4
Ta7ar <56931453+Ta7ar@users.noreply.github.com>
2020-06-16 00:49:10 -0400

    build script run locally

---
+++ client/build/precache-manifest.3b9015f3f0b402125ebf6d7fa293e8a1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ede3d46b0f46e1a7ddc9647322e5b5f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dba765243ad6382003de",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "a67a44b41abd48af959c",
+    "url": "/static/css/main.f5b5de19.chunk.css"
+  },
+  {
+    "revision": "dba765243ad6382003de",
+    "url": "/static/js/2.3f9a02ce.chunk.js"
+  },
+  {
+    "revision": "4b6ddcc7458664c42c0d6c9ea41a632f",
+    "url": "/static/js/2.3f9a02ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a67a44b41abd48af959c",
+    "url": "/static/js/main.bd6935d4.chunk.js"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  }
+]);
